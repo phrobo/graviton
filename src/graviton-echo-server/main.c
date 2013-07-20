fcf057935a9c5e301ed0b38635665f81d53dce3a
Torrie Fischer <tdfischer@phrobo.net>
2013-07-20 10:26:56 -0400

    Write a simple echo server as proof of concept

--- src/graviton-echo-server/main.c
+++ src/graviton-echo-server/main.c
@@ -1,6 +1,13 @@
 #include <graviton/server.h>
+#include <graviton/control.h>
 
 #include <glib.h>
+
+static GVariant *
+cb_ping(GravitonControl *control, GHashTable *args, GError **error, gpointer user_data)
+{
+  return g_variant_new_string ("pong");
+}
 
 int main(int argc, char** argv)
 {
@@ -12,8 +19,9 @@ int main(int argc, char** argv)
   loop = g_main_loop_new (NULL, FALSE);
 
   server = graviton_server_new ();
+  GravitonRootControl *root = graviton_server_get_root_control (server);
 
-  graviton_server_load_plugins (server);
+  graviton_control_add_method (GRAVITON_CONTROL (root), "ping", cb_ping, NULL, NULL);
 
   graviton_server_run_async (server);
 
