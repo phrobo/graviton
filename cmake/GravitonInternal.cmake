45e32e9f21fa7c8219ea19fc6eba7f7025f6d35e
Torrie Fischer <tdfischer@hackerbots.net>
2014-11-02 17:01:55 -0800

    Tweak target naming when passed to add_graviton_*_plugin

--- cmake/GravitonInternal.cmake
+++ cmake/GravitonInternal.cmake
@@ -1,37 +1,26 @@
 set(GRAVITON_VERSION
   "${GRAVITON_VERSION_MAJOR}.${GRAVITON_VERSION_MINOR}.${GRAVITON_VERSION_PATCH}")
 
-function(add_graviton_plugin)
-  cmake_parse_arguments(_plugin "" "NAME" "SOURCES" ${ARGN})
-  add_library(graviton-plugin-${_plugin_NAME} MODULE ${_plugin_SOURCES})
-  string(REPLACE ";" " " _LDFLAGS "${GRAVITON_LDFLAGS};${GRAVITON_LDFLAGS_OTHER}")
-  set_target_properties(graviton-plugin-${_plugin_NAME} PROPERTIES
-    OUTPUT_NAME ${_plugin_NAME}
-    PREFIX ""
-    LINK_FLAGS "${_LDFLAGS}"
-  )
-endfunction(add_graviton_plugin)
-
 function(add_graviton_discovery_plugin)
   cmake_parse_arguments(_plugin "" "NAME" "SOURCES" ${ARGN})
-  message (STATUS "graviton-discovery-plugin-${_plugin_NAME}")
-  add_library(graviton-discovery-plugin-${_plugin_NAME} MODULE ${_plugin_SOURCES})
+  add_library(${_plugin_NAME} MODULE ${_plugin_SOURCES})
   string(REPLACE ";" " " _LDFLAGS "${GRAVITON_LDFLAGS};${GRAVITON_LDFLAGS_OTHER}")
-  set_target_properties(graviton-discovery-plugin-${_plugin_NAME} PROPERTIES
+  set_target_properties(${_plugin_NAME} PROPERTIES
     OUTPUT_NAME ${_plugin_NAME}
     PREFIX ""
     LINK_FLAGS "${_LDFLAGS}"
   )
+  target_link_libraries(${_plugin_NAME} graviton-client)
 endfunction(add_graviton_discovery_plugin)
 
 function(add_graviton_publish_plugin)
   cmake_parse_arguments(_plugin "" "NAME" "SOURCES" ${ARGN})
-  message (STATUS "graviton-publish-plugin-${_plugin_NAME}")
-  add_library(graviton-publish-plugin-${_plugin_NAME} MODULE ${_plugin_SOURCES})
+  add_library(${_plugin_NAME} MODULE ${_plugin_SOURCES})
   string(REPLACE ";" " " _LDFLAGS "${GRAVITON_LDFLAGS};${GRAVITON_LDFLAGS_OTHER}")
-  set_target_properties(graviton-publish-plugin-${_plugin_NAME} PROPERTIES
+  set_target_properties(${_plugin_NAME} PROPERTIES
     OUTPUT_NAME ${_plugin_NAME}
     PREFIX ""
     LINK_FLAGS "${_LDFLAGS}"
   )
+  target_link_libraries(${_plugin_NAME} graviton-server)
 endfunction(add_graviton_publish_plugin)
